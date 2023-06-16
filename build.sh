47a87d8334b3a7aff07c6f25ab2a4bf5b98f9b2b
Jason Umiker <jason.umiker@sysdig.com>
2023-06-16 15:33:47 +1000

    Don't overwrite the latest tag which is in use

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ docker buildx build --push \
   --platform linux/arm64,linux/amd64 \
   --tag public.ecr.aws/m9h2b5e7/security-playground:110623 \
   .
-docker buildx build --push \
-  --platform linux/arm64,linux/amd64 \
-  --tag public.ecr.aws/m9h2b5e7/security-playground:latest \
-  .
+#docker buildx build --push \
+#  --platform linux/arm64,linux/amd64 \
+#  --tag public.ecr.aws/m9h2b5e7/security-playground:latest \
+#  .
 docker buildx rm mybuilder
