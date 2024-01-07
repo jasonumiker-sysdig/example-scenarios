a67a1f28928a0ed0a19116b8b9a29d633be444a2
Jason Umiker <jason.umiker@sysdig.com>
2024-01-08 09:41:56 +1100

    Updated hello apps and postgres-sakila

--- docker-build-hello-app/build.sh
+++ docker-build-hello-app/build.sh
@@ -1,10 +1,10 @@
 docker buildx create --name mybuilder --bootstrap --use
 docker buildx build --push \
   --platform linux/arm64,linux/amd64 \
-  --tag public.ecr.aws/m9h2b5e7/hello-app:270723 \
+  --tag public.ecr.aws/m9h2b5e7/hello-app:080124 \
   .
-docker buildx build --push \
-  --platform linux/arm64,linux/amd64 \
-  --tag public.ecr.aws/m9h2b5e7/hello-app:latest \
-  .
+#docker buildx build --push \
+#  --platform linux/arm64,linux/amd64 \
+#  --tag public.ecr.aws/m9h2b5e7/hello-app:latest \
+#  .
 docker buildx rm mybuilder
