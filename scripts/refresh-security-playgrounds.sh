a6b09d91b36313e698eede4be548629a84ce3dc9
Jason Umiker <jason.umiker@sysdig.com>
2024-03-18 08:59:43 +1100

    Fixed typo

--- scripts/refresh-security-playgrounds.sh
+++ scripts/refresh-security-playgrounds.sh
@@ -3,3 +3,4 @@ kubectl delete --all pods --namespace=security-playground
 kubectl delete --all pods --namespace=security-playground-restricted
 kubectl delete --all pods --namespace=security-playground-restricted-nodrift
 kubectl delete --all pods --namespace=security-playground-restricted-nomalware
+kubectl delete deployment nefarious-workload -n security-playground
--- scripts/test-all-workshop-commands.sh
+++ scripts/test-all-workshop-commands.sh
@@ -1,17 +1,18 @@
+#!/bin/bash
 ./example-curls.sh
 ./example-curls-restricted.sh
 ./example-curls-restricted-nodrift.sh
 ./example-curls-restricted-nomalware.sh
-kubectl apply -f ./security-playground-irsa.yaml
-sleep 10
-./example-curls-bucket-public.sh
+#kubectl apply -f ./security-playground-irsa.yaml
+#sleep 10
+#./example-curls-bucket-public.sh
 ./sysdig-cli-scanner -a app.au1.sysdig.com logstash:7.16.1
 ./example-curls-networkpolicy.sh
-kubectl apply -f ./generated-network-policy.yaml
+kubectl apply -f ./generated-network-policy.yml
 sleep 10
 ./example-curls-networkpolicy.sh
 kubectl logs deployment/hello-client-blocked -n hello
 kubectl logs deployment/hello-client -n hello
-kubectl apply -f ./generated-network-policy2.yaml
+kubectl apply -f ./generated-network-policy2.yml
 sleep 10
 ./example-curls.sh
