1622f419ed9c209f5a9d8f6adbc15c044372ee95
Jason Umiker <jason.umiker@sysdig.com>
2024-03-20 17:53:39 +1100

    Updated refresh to delete nefarious earlier

--- scripts/refresh-security-playgrounds.sh
+++ scripts/refresh-security-playgrounds.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+kubectl delete deployment nefarious-workload -n security-playground
 kubectl delete --all pods --namespace=security-playground
 kubectl delete --all pods --namespace=security-playground-restricted
 kubectl delete --all pods --namespace=security-playground-restricted-nodrift
 kubectl delete --all pods --namespace=security-playground-restricted-nomalware
-kubectl delete deployment nefarious-workload -n security-playground
+kubectl delete -f ./generated-network-policy.yml
+kubectl delete -f ./generated-network-policy2.yml
