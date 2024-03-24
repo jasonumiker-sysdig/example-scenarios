d16ade1e042e034f89fb3c268aac0f2ca0d7c30a
=Jason Umiker <jason@computer>
2024-03-24 15:10:19 +1100

    Updated all the ECR Public Images

--- docker-build-postgres-sakila/build.sh
+++ docker-build-postgres-sakila/build.sh
@@ -2,10 +2,10 @@
 docker buildx create --name mybuilder --bootstrap --use
 docker buildx build --push \
   --platform linux/arm64,linux/amd64 \
-  --tag public.ecr.aws/m9h2b5e7/postgres-sakila:080124 \
+  --tag public.ecr.aws/m9h2b5e7/postgres-sakila:240324 \
   .
-#docker buildx build --push \
-#  --platform linux/arm64,linux/amd64 \
-#  --tag public.ecr.aws/m9h2b5e7/postgres-sakila:latest \
-#  .
+docker buildx build --push \
+  --platform linux/arm64,linux/amd64 \
+  --tag public.ecr.aws/m9h2b5e7/postgres-sakila:latest \
+  .
 docker buildx rm mybuilder
--- docker-build-security-playground/build.sh
+++ docker-build-security-playground/build.sh
@@ -2,10 +2,10 @@
 docker buildx create --name mybuilder --bootstrap --use
 docker buildx build --push \
   --platform linux/arm64,linux/amd64 \
-  --tag public.ecr.aws/m9h2b5e7/security-playground:070124 \
+  --tag public.ecr.aws/m9h2b5e7/security-playground:240324 \
   .
-#docker buildx build --push \
-#  --platform linux/arm64,linux/amd64 \
-#  --tag public.ecr.aws/m9h2b5e7/security-playground:latest \
-#  .
+docker buildx build --push \
+  --platform linux/arm64,linux/amd64 \
+  --tag public.ecr.aws/m9h2b5e7/security-playground:latest \
+  .
 docker buildx rm mybuilder
